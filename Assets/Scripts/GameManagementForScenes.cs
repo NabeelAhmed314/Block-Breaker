9d340dc5d84c6277911a3cf9c87227f58a4c5ca3
Nabeel Ahmed <nabeelrajpoot314@gmail.com>
2020-09-07 11:24:42 +0500

    first

---
+++ Assets/Scripts/GameManagementForScenes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManagementForScenes : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Level Start: "+ PlayerPrefs.GetInt("Levels"));
+        SceneManager.LoadScene("Level" + PlayerPrefs.GetInt("Levels"));
+    }
+    public void ResetGame()
+    {
+        Debug.Log("Reset Level To : " + PlayerPrefs.GetInt("Levels"));
+        PlayerPrefs.SetInt("Levels", 1);
+    }
+}
---
+++ Assets/Scripts/Levels.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levels : MonoBehaviour
+{
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("Levels") <= 0)
+        {
+            PlayerPrefs.SetInt("Levels", 1);
+        }
+    }
+}
